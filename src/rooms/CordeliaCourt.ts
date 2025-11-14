28617f8b238b329b4962ee8dfbb459c47d79121e
occiandiaali <occiandiaali@gmail.com>
2025-11-14 17:24:47 +0100

    Debug max clients 2

--- src/rooms/CordeliaCourt.ts
+++ src/rooms/CordeliaCourt.ts
@@ -77,12 +77,12 @@ export class CordeliaCourt extends Room<CordeliaCourtState> {
 
   onJoin(client: Client, options: any) {
     // Enforce 2-player limit
-    if (this.clients.length > 2) {
-      console.log("Room full, rejecting client:", client.sessionId);
-      client.leave(4000, "Room is full (max 2 users).");
+    // if (this.clients.length > 2) {
+    //   console.log("Room full, rejecting client:", client.sessionId);
+    //   client.leave(4000, "Room is full (max 2 users).");
 
-      return;
-    }
+    //   return;
+    // }
 
     // Each client can pass their own username
     const username = options.currentUser;
