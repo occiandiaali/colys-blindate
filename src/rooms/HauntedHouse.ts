4779ac15cd5224f7a52d314f09967628e4721a77
occiandiaali <occiandiaali@gmail.com>
2025-12-09 16:01:11 +0100

    Add countdown timer with format text

--- src/rooms/HauntedHouse.ts
+++ src/rooms/HauntedHouse.ts
@@ -25,7 +25,8 @@ export class HauntedHouse extends Room<HauntedHouseState> {
     //roomid, timer, me, participants -> options
 
     // const timer = options.timer || 60;
-    this.state.timeLimit = options.timer * 60 || 300; // Default 300 seconds (5mins)
+    //  this.state.timeLimit = options.timer * 60 || 300; // Default 300 seconds (5mins)
+    this.state.timeLimit = options.timer || 300; // Default 300 seconds (5mins)
 
     // Setup schema with players map...
     this.onMessage("updatePosition", (client, data) => {
@@ -62,15 +63,28 @@ export class HauntedHouse extends Room<HauntedHouseState> {
       this.broadcast("gameStarted", this.state.gameStarted);
       // Countdown loop
       const interval = setInterval(() => {
-        this.state.timeLimit--;
-        this.broadcast("remainingTime", this.state.timeLimit);
+        //  this.state.timeLimit--;
+        let remaining = this.state.timeLimit * 60; // mins to seconds
+        // format as mm:ss
+        const minutes = Math.floor(this.state.timeLimit / 60);
+        const seconds = remaining % 60;
+        const formatted = `${minutes}:${seconds.toString().padStart(2, "0")}`;
+        //this.broadcast("remainingTime", this.state.timeLimit);
+        this.broadcast("remainingTime", formatted);
 
-        if (this.state.timeLimit <= 0) {
+        // if (this.state.timeLimit <= 0) {
+        //   this.state.gameOver = true;
+        //   clearInterval(interval);
+        //   this.disconnect();
+        //   this.broadcast("gameOver", this.state.gameOver);
+        // }
+        if (remaining <= 0) {
           this.state.gameOver = true;
           clearInterval(interval);
           this.disconnect();
           this.broadcast("gameOver", this.state.gameOver);
         }
+        remaining--;
       }, 1000);
     }
   }
